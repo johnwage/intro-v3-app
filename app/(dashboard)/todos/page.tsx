892d1c4883444af9260d2ec8ad76f46d8cfc4e8e
John Wage <john.m.wage@gmail.com>
2024-06-11 14:35:58 -0400

    Add Prisma folders from complete version. Basic Todo and TodoList complete for display in components

--- app/(dashboard)/todos/page.tsx
+++ app/(dashboard)/todos/page.tsx
@@ -1,5 +1,18 @@
-const ToDosPage = () => {
-  return <div>To Dos</div>
+import db from '@/utils/db'
+import TodoList from '@/components/TodoList'
+
+const getData = async () => {
+  const todos = await db.todo.findMany({})
+  return todos
+}
+
+const ToDosPage = async () => {
+  const todos = await getData()
+  return (
+    <div>
+      <TodoList todos={todos} />
+    </div>
+  )
 }
 
 export default ToDosPage
