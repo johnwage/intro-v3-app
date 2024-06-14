060431885170da352a0aec46faf998d37182fe84
John Wage <john.m.wage@gmail.com>
2024-06-14 14:52:32 -0400

    Complete server action mutation

--- utils/actions.ts
+++ utils/actions.ts
@@ -2,6 +2,19 @@
 import { revalidatePath } from 'next/cache'
 import db from './db'
 
+export const completeTodo = async (id) => {
+  await db.todo.update({
+    where: { id },
+    data: {
+      completed: true,
+    },
+  })
+
+  // makes the todos page reload after a new todo is created in db
+  // expires the cache of that page, which forces it to refetch its data
+  revalidatePath('/todos')
+}
+
 export const newToDo = async (formData) => {
   const todo = await db.todo.create({
     data: {
